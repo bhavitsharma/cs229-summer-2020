6eba4ffa1a391fee74feb8fc7c73c5b2708cff40
Bhavit Sharma <bhavit.sharma804@gmail.com>
2023-04-24 07:01:08 +0530

    Add solutions till 3.

--- pset1/ps1/src/poisson/poisson.py
+++ pset1/ps1/src/poisson/poisson.py
@@ -2,8 +2,9 @@ import numpy as np
 import util
 import matplotlib.pyplot as plt
 
+
 def main(lr, train_path, eval_path, save_path):
-    """Problem: Poisson regression with gradient ascent.
+  """Problem: Poisson regression with gradient ascent.
 
     Args:
         lr: Learning rate for gradient ascent.
@@ -11,17 +12,26 @@ def main(lr, train_path, eval_path, save_path):
         eval_path: Path to CSV file containing dataset for evaluation.
         save_path: Path to save predictions.
     """
-    # Load training set
-    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+  # Load training set
+  x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+  x_valid, y_valid = util.load_dataset(eval_path, add_intercept=True)
 
-    # *** START CODE HERE ***
-    # Fit a Poisson Regression model
-    # Run on the validation set, and use np.savetxt to save outputs to save_path
-    # *** END CODE HERE ***
+  # Train poisson model
+  reg = PoissonRegression(step_size=lr)
+  reg.fit(x_train, y_train)
+  preds = reg.predict(x_valid)
+  np.savetxt(save_path, preds)
+
+  # plot predictions
+  plt.scatter(y_valid, preds)
+  plt.ylabel('Found Count')
+  plt.xlabel('Expected count')
+  plt.axis('equal')
+  plt.savefig('poisson.png')
 
 
 class PoissonRegression:
-    """Poisson Regression.
+  """Poisson Regression.
 
     Example usage:
         > clf = PoissonRegression(step_size=lr)
@@ -29,9 +39,13 @@ class PoissonRegression:
         > clf.predict(x_eval)
     """
 
-    def __init__(self, step_size=1e-5, max_iter=10000000, eps=1e-5,
-                 theta_0=None, verbose=True):
-        """
+  def __init__(self,
+               step_size=1e-5,
+               max_iter=10000000,
+               eps=1e-5,
+               theta_0=None,
+               verbose=True):
+    """
         Args:
             step_size: Step size for iterative solvers only.
             max_iter: Maximum number of iterations for the solver.
@@ -39,24 +53,49 @@ class PoissonRegression:
             theta_0: Initial guess for theta. If None, use the zero vector.
             verbose: Print loss values during training.
         """
-        self.theta = theta_0
-        self.step_size = step_size
-        self.max_iter = max_iter
-        self.eps = eps
-        self.verbose = verbose
+    self.theta = theta_0
+    self.step_size = step_size
+    self.max_iter = max_iter
+    self.eps = eps
+    self.verbose = verbose
 
-    def fit(self, x, y):
-        """Run gradient ascent to maximize likelihood for Poisson regression.
+  def y_hat(self, x):
+    # The rule is y_hat = exp(theta^T * x)
+    # theta is a column vector (5 x 1)
+    # x is matrix (n_examples x 5)
+    return np.exp(x @ self.theta)
+
+  def gradient(self, x, y):
+    # The rule is theta_j = theta_j + learning_rate * (y - y_hat) * x_j
+    f = (y - self.y_hat(x))
+    return x.T @ f
+
+  def fit(self, x, y):
+    """Run gradient ascent to maximize likelihood for Poisson regression.
 
         Args:
             x: Training example inputs. Shape (n_examples, dim).
             y: Training example labels. Shape (n_examples,).
         """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
+    if self.theta is None:
+      self.theta = np.zeros(x.shape[1])
+      # Fix the column dimension of theta
+      self.theta = np.reshape(self.theta, (self.theta.shape[0], 1))
+      y = np.reshape(y, (y.shape[0], 1))
+      print(self.theta.shape, x.shape)
 
-    def predict(self, x):
-        """Make a prediction given inputs x.
+    iteration = 0
+    while iteration < self.max_iter:
+      iteration += 1
+      gradient = self.gradient(x, y)
+      last_theta = np.copy(self.theta)
+      self.theta += self.step_size * gradient
+      if np.linalg.norm(self.theta - last_theta) < self.eps:
+        break
+    print('Converged after %d iterations.' % iteration)
+
+  def predict(self, x):
+    """Make a prediction given inputs x.
 
         Args:
             x: Inputs of shape (n_examples, dim).
@@ -64,11 +103,12 @@ class PoissonRegression:
         Returns:
             Floating-point prediction for each input, shape (n_examples,).
         """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
+    # Since the mean of Poisson distribution is y_hat, we can just return y_hat
+    return self.y_hat(x)
+
 
 if __name__ == '__main__':
-    main(lr=1e-5,
-        train_path='train.csv',
-        eval_path='valid.csv',
-        save_path='poisson_pred.txt')
+  main(lr=1e-5,
+       train_path='train.csv',
+       eval_path='valid.csv',
+       save_path='poisson_pred.txt')
--- pset1/ps1/src/posonly/posonly.py
+++ pset1/ps1/src/posonly/posonly.py
@@ -13,7 +13,7 @@ WILDCARD = 'X'
 
 
 def main(train_path, valid_path, test_path, save_path):
-    """Problem 2: Logistic regression for incomplete, positive-only labels.
+  """Problem 2: Logistic regression for incomplete, positive-only labels.
 
     Run under the following conditions:
         1. on t-labels,
@@ -26,21 +26,41 @@ def main(train_path, valid_path, test_path, save_path):
         test_path: Path to CSV file containing test set.
         save_path: Path to save predictions.
     """
-    output_path_true = save_path.replace(WILDCARD, 'true')
-    output_path_naive = save_path.replace(WILDCARD, 'naive')
-    output_path_adjusted = save_path.replace(WILDCARD, 'adjusted')
+  output_path_true = save_path.replace(WILDCARD, 'true')
+  output_path_naive = save_path.replace(WILDCARD, 'naive')
+  output_path_adjusted = save_path.replace(WILDCARD, 'adjusted')
 
-    # *** START CODE HERE ***
-    # Part (a): Train and test on true labels
-    # Make sure to save predicted probabilities to output_path_true using np.savetxt()
-    # Part (b): Train on y-labels and test on true labels
-    # Make sure to save predicted probabilities to output_path_naive using np.savetxt()
-    # Part (f): Apply correction factor using validation set and test on true labels
-    # Plot and use np.savetxt to save outputs to output_path_adjusted
-    # *** END CODER HERE
+  # *** START CODE HERE ***
+  # Part (a): Train and test on true labels
+  x_train, t_train = util.load_dataset(train_path, 't', add_intercept=True)
+  x_test, t_test = util.load_dataset(test_path, 't', add_intercept=True)
+  clf = LogisticRegression()
+  clf.fit(x_train, t_train)
+  util.plot(x_test, t_test, clf.theta, 'posonly-true.jpg')
+  np.savetxt(output_path_true, clf.predict(x_test))
+
+  # Make sure to save predicted probabilities to output_path_true using np.savetxt()
+  # Part (b): Train on y-labels and test on true labels
+  x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+  x_test, y_test = util.load_dataset(test_path, add_intercept=True)
+  x_valid, y_valid = util.load_dataset(valid_path, add_intercept=True)
+  clf = LogisticRegression()
+  clf.fit(x_train, y_train)
+  util.plot(x_test, t_test, clf.theta, 'posonly-naive.jpg')
+  np.savetxt(output_path_naive, clf.predict(x_test))
+
+  # Make sure to save predicted probabilities to output_path_naive using np.savetxt()
+  # Part (f): Apply correction factor using validation set and test on true labels
+  alpha = np.mean(clf.predict(x_valid[y_valid == 1]))
+  np.savetxt(output_path_adjusted, clf.predict(x_test) / alpha)
+  # Since we're scaling it by alpha, we need to divide by alpha
+  # And theta becomes 1 / alpha * e^(-theta^T x) = 1  / e^(-theta^T x + log(alpha))
+  clf.theta[0] += np.log(2 / alpha - 1)
+  util.plot(x_test, t_test, clf.theta, 'posonly_adjusted.jpg')
+
 
 if __name__ == '__main__':
-    main(train_path='train.csv',
-        valid_path='valid.csv',
-        test_path='test.csv',
-        save_path='posonly_X_pred.txt')
+  main(train_path='train.csv',
+       valid_path='valid.csv',
+       test_path='test.csv',
+       save_path='posonly_X_pred.txt')
